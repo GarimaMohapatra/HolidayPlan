fb44bdb260f83d529eff9fe48a62247a01653c64
Saurav <sauravmishraid@gmail.com>
2019-10-27 21:28:28 +1100

    Implemented viewModel

--- ViewModel/HolidayPlanningViewModel.swift
+++ ViewModel/HolidayPlanningViewModel.swift
@@ -7,59 +7,67 @@
 //
 
 import Foundation
-protocol HolidayPlanningViewModelRules {
+protocol UrlConfiguring: AnyObject {
     var url: String { get }
-    var dic: Dictionary<String,String> { get }
+    var dic: [String: String] { get }
+}
+protocol ResponseModeling {
     var model: Decodable.Type { get  }
+}
+protocol RequestModeling {
+    var userEntry: String { get  }
+}
+protocol HeaderConfiguration {
     var componentScheme: String { get  }
     var componenthost: String { get }
     var appendcomponent: String { get  }
-    var userEntry: String { get  }
+}
+protocol KeyValueConfiguration {
     var token: String? { get  }
     var value: String? { get  }
-    
 }
-class HolidayPlanningViewModel: HolidayPlanningViewModelRules {
-    
+typealias UrlConfigurable = UrlConfiguring & RequestModeling & RequestModeling & HeaderConfiguration & KeyValueConfiguration
+
+class HolidayPlanningViewModel: UrlConfigurable {
+
     var url: String {
-        return "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/example"    }
-    
-    var dic: Dictionary<String, String> {
+        return componentScheme + componenthost + appendcomponent + userEntry
+    }
+
+        var dic: [String: String] {
         return ["app_id": "ac5f4204", "app_key": "188096a142883e86eee4ef848845411c"]    }
-    
+
     var model: Decodable.Type {
         return OxfordDictonary.self
-        
+
     }
-    
+
     var componentScheme: String {
         return "https"
-        
+
     }
-    
+
     var componenthost: String {
         return "od-api.oxforddictionaries.com"
-        
+
     }
-    
+
     var appendcomponent: String {
         "/api/v2/entries/en-gb"
-        
+
     }
-    
+
     var userEntry: String {
         return "/love"
-        
+
     }
-    
+
     var token: String? {
         return nil
     }
-    
+
     var value: String? {
         return nil
     }
-    
-  
-    
+
 }
